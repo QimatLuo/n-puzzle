8d57f6741d6ccd173f884f42c6d8611f1db41482
Qimat Luo <qimatluo@gmail.com>
2022-06-19 01:00:23 +0800

    Implement functions

---
+++ __tests__/integration.ts
@@ -0,0 +1,101 @@
+import { getMovePos } from "../src/n-puzzle";
+import { swap } from "../src/util";
+
+describe("play", () => {
+  let tiles = [
+    { id: 3, ui: "" },
+    { id: 2, ui: "c" },
+    { id: 1, ui: "b" },
+    { id: 0, ui: "a" },
+  ];
+
+  let pos: number;
+
+  test.each([
+    [
+      0,
+      [
+        { id: 3, ui: "" },
+        { id: 2, ui: "c" },
+        { id: 1, ui: "b" },
+        { id: 0, ui: "a" },
+      ],
+    ],
+    [
+      1,
+      [
+        { id: 2, ui: "c" },
+        { id: 3, ui: "" },
+        { id: 1, ui: "b" },
+        { id: 0, ui: "a" },
+      ],
+    ],
+    [
+      2,
+      [
+        { id: 2, ui: "c" },
+        { id: 3, ui: "" },
+        { id: 1, ui: "b" },
+        { id: 0, ui: "a" },
+      ],
+    ],
+    [
+      3,
+      [
+        { id: 2, ui: "c" },
+        { id: 0, ui: "a" },
+        { id: 1, ui: "b" },
+        { id: 3, ui: "" },
+      ],
+    ],
+    [
+      4,
+      [
+        { id: 2, ui: "c" },
+        { id: 0, ui: "a" },
+        { id: 1, ui: "b" },
+        { id: 3, ui: "" },
+      ],
+    ],
+    [
+      2,
+      [
+        { id: 2, ui: "c" },
+        { id: 0, ui: "a" },
+        { id: 3, ui: "" },
+        { id: 1, ui: "b" },
+      ],
+    ],
+    [
+      0,
+      [
+        { id: 3, ui: "" },
+        { id: 0, ui: "a" },
+        { id: 2, ui: "c" },
+        { id: 1, ui: "b" },
+      ],
+    ],
+    [
+      1,
+      [
+        { id: 0, ui: "a" },
+        { id: 3, ui: "" },
+        { id: 2, ui: "c" },
+        { id: 1, ui: "b" },
+      ],
+    ],
+    [
+      3,
+      [
+        { id: 0, ui: "a" },
+        { id: 1, ui: "b" },
+        { id: 2, ui: "c" },
+        { id: 3, ui: "" },
+      ],
+    ],
+  ])("%s", (x, r) => {
+    pos = getMovePos(tiles)(x);
+    tiles = swap(tiles)(x)(pos);
+    expect(tiles).toEqual(r);
+  });
+});
---
+++ __tests__/n-puzzle.ts
@@ -0,0 +1,87 @@
+import {
+  customTiles,
+  getMovePos,
+  numberTiles,
+  verifyTiles,
+} from "../src/n-puzzle";
+
+describe("customTiles", () => {
+  test.each([
+    [
+      ["a", "b", "c"],
+      [
+        { id: 0, ui: "a" },
+        { id: 1, ui: "b" },
+        { id: 2, ui: "c" },
+        { id: 3, ui: "" },
+      ],
+    ],
+    [
+      ["1", "2", "3"],
+      [
+        { id: 0, ui: "1" },
+        { id: 1, ui: "2" },
+        { id: 2, ui: "3" },
+        { id: 3, ui: "" },
+      ],
+    ],
+  ])("%s", (x, r) => {
+    expect(customTiles(x)).toEqual(r);
+  });
+});
+
+describe("getMovePos", () => {
+  const xs = numberTiles(2);
+  const f = getMovePos(xs);
+
+  test.each([
+    [0, undefined],
+    [1, 3],
+    [2, 3],
+    [3, undefined],
+  ])("%s", (x, r) => {
+    expect(f(x)).toEqual(r);
+  });
+});
+
+describe("numberTiles", () => {
+  test.each([
+    [
+      2,
+      [
+        { id: 0, ui: "1" },
+        { id: 1, ui: "2" },
+        { id: 2, ui: "3" },
+        { id: 3, ui: "" },
+      ],
+    ],
+    [-1, []],
+  ])("%s", (x, r) => {
+    expect(numberTiles(x)).toEqual(r);
+  });
+});
+
+describe("verifyTiles", () => {
+  test.each([
+    [
+      true,
+      [
+        { id: 0, ui: "a" },
+        { id: 1, ui: "b" },
+        { id: 2, ui: "c" },
+        { id: 3, ui: "" },
+      ],
+    ],
+    [
+      false,
+      [
+        { id: 0, ui: "a" },
+        { id: 1, ui: "b" },
+        { id: 3, ui: "" },
+        { id: 2, ui: "c" },
+      ],
+    ],
+  ])("%s", (r, x) => {
+    expect(verifyTiles(x)).toEqual(r);
+  });
+});
